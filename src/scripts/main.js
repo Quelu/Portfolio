7e3dc38b6d18d8681b2d4ca24cd288356b3c1a93
Luc Matagne <quelumatagne@gmail.com>
2015-09-17 18:55:06 +0200

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,106 @@
+window.$ = require('jquery');
+var wowjs = require('wowjs');
+
+
+$(function(){
+  var wow = new wowjs.WOW();
+
+  /**
+   * media-query
+   **/
+
+
+  if ( $( window ).width() > 768 ) {
+    // If screen is medium
+    $( '.chart__data' ).addClass( 'wow' ).addClass( 'slideInLeft' );
+    $( '.chart__data-bar' ).addClass( 'wow' ).removeClass( 'slideInLeft' );
+  } else {
+    // Screen is small
+    $( '.chart__data' ).addClass( 'wow' ).removeClass( 'slideInLeft' );
+    $( '.chart__data-bar' ).addClass( 'wow' ).addClass( 'slideInLeft' );
+  }
+
+  wow.init();
+
+
+  $( 'body' ).removeClass( 'hidden' ).addClass( 'fadeIn' );
+
+  /**
+   * HEADER
+   **/
+
+  $( '.slider__item').hide();
+  $( '.slider__item:first-child').show();
+
+  $( '.header__icons' ).show();
+
+  $( '.header__icon-link' ).click(function ( e ) {
+    e.preventDefault();
+  });
+
+  $( '.header__icon-link' ).hover( function () {
+    changeSelectedIconLink( this );
+  } );
+
+  $( '.header__icon-link' ).focus( function () {
+    changeSelectedIconLink( this );
+  } );
+
+  var changeSelectedIconLink = function ( el ) {
+    $( '.header__icon-link' ).removeClass( 'header__icon-link--activated' );
+    $( el ).addClass( 'header__icon-link--activated' );
+
+    $( '.slider__item').hide();
+    $( '.slider__item').eq( el.dataset.n ).show();
+
+    $( '.header' ).removeClass( 'header--dev header--graph header--photo' );
+    $( '.header' ).addClass( 'header--' + el.dataset.name );
+  };
+
+
+  /**
+   * Menu
+   **/
+
+   $( '#openMenu' ).click( function () {
+     $( '.navigation' ).toggleClass( 'navigation--open' );
+   } );
+
+  var menu = $('.menu'),
+  	menuTopPosition = menu.offset().top;
+
+  $(window).on('scroll', function(){
+  	if( $(window).scrollTop() > menuTopPosition ) {
+  		menu.addClass('menu--sticked');
+      $( '#main' ).css( 'padding-top', '3.75em' );
+  	} else {
+  		menu.removeClass('menu--sticked');
+      $( '#main' ).css( 'padding-top', '0' );
+  	}
+  });
+
+  $( '.navigation__link' ).click( function () {
+    $( '.navigation' ).removeClass( 'navigation--open' );
+    $( '#main' ).css( 'padding-top', '0' );
+  } );
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        if ( target[0].id == 'section-contact' ) {
+          $("html, body").animate({
+            scrollTop: $(document).height()
+          }, 750);
+        } else {
+          $('html,body').animate({
+            scrollTop: target.offset().top - $('.menu').height() - 100
+          }, 500);
+        }
+        return false;
+      }
+    }
+  });
+
+});
